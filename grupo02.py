5d07656ea96282092ed05bb9f1861393cae31652
NicoMalta <niicou96@hotmail.com>
2018-06-03 16:20:32 -0300

    Tincho

--- grupo02.py
+++ grupo02.py
@@ -1,42 +1,45 @@
-seguir = True
 lista_gramatica = []
 
 class ReglaGramatica():
-    reglaGramatica = []
-    terminales = []
-    noTerminales = []
-    follows = []
-    firsts = []
-    selects = []
-    EsLL1 = bool
+
+    def __init__(self):
+        self.reglaGramatica = []
+        self.terminales = []
+        self.noTerminales = []
+        self.follows = []
+        self.firsts = []
+        self.selects = []
 
 
 
 def setear_gramatica(gramatica_str):
-    Reglas = ReglaGramatica()
+    Regla = ReglaGramatica()
+    i = 0
+    while i < len(gramatica_str):
+        if gramatica_str[i] == "\n" or i == len(gramatica_str)-1:
+            lista_gramatica.append(Regla)
+            Regla = ReglaGramatica()
+            print(len(lista_gramatica))
+        else:
+            Regla.reglaGramatica.append(gramatica_str[i])
+        
+        char = gramatica_str[i]
+        if char.isupper() == True:
+            Regla.noTerminales.append(char)
+        elif char != '' and char != ':' and char !=' ' and char !='\n':
+            f = i+1
+            while gramatica_str[f] !=' ' and f < len(gramatica_str):
+                char += gramatica_str[f] 
+                f += 1
+            if f < len(gramatica_str):
+                Regla.terminales.append(char)
+                i += (f - i)
+        i += 1
+    input()
+
     
 
 
 
-param_gramatica = input()
-print(setear_gramatica(param_gramatica))
-
-
-
-'''stack = ['E']
-look = yylex()
-
-while stack:
-    s = stack.pop()
-    if s in NT:
-        l = tale[(s, look)]
-        l = l[::-1]
-        sack.extend(1)
-    elif s == look:
-        look = yylex()
-    else:
-        print('Error')
-if look == '$':
-    print('OK')
-else:
-    print('Error')'''
+'''param_gramatica = input()'''
+print(setear_gramatica("A : b A \n A : a \n A : A B c \n A : lambda \n B : b"))
