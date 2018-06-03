0cf4d8777c96cdbfbeb05ad87ef6fdbf2431eff2
NicoMalta <niicou96@hotmail.com>
2018-06-03 19:42:47 -0300

    Terminales y no terminales finish

--- grupo02.py
+++ grupo02.py
@@ -14,27 +14,37 @@ class ReglaGramatica():
 
 def setear_gramatica(gramatica_str):
     Regla = ReglaGramatica()
-    i = 0
+    i = 0 
+    band = False
     while i < len(gramatica_str):
-        if gramatica_str[i] == "\n" or i == len(gramatica_str)-1:
+        if gramatica_str[i] == "\n" or i == len(gramatica_str):
             lista_gramatica.append(Regla)
             Regla = ReglaGramatica()
-            print(len(lista_gramatica))
         else:
-            Regla.reglaGramatica.append(gramatica_str[i])
-        
+            if band == False:
+                Regla.reglaGramatica.append(gramatica_str[i])
+            else:
+                Regla.reglaGramatica.append(char)
+                band = False
+      
         char = gramatica_str[i]
         if char.isupper() == True:
             Regla.noTerminales.append(char)
         elif char != '' and char != ':' and char !=' ' and char !='\n':
-            if  i+1 < len(gramatica_str):
-                f = i+1
+            f = i+1
+            if f == len(gramatica_str) and gramatica_str[f-2] != " ":
+                char += gramatica_str[f] 
+            elif f != len(gramatica_str):
                 while gramatica_str[f] != ' ':
                     char += gramatica_str[f] 
                     f += 1
-                if f < len(gramatica_str):
-                    Regla.terminales.append(char)
-                    i += ((f-1) - i)
+                    band = True
+            if f <= len(gramatica_str):
+                Regla.terminales.append(char)
+                i += ((f-1) - i)
+                if f == len(gramatica_str):
+                    lista_gramatica.append(Regla)
+                    Regla = ReglaGramatica()
         i += 1
 
         """Falta el ulimo caracter , ver porque se pierde"""
