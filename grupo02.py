9b22cc7a8d15be5ddff7bda4f7190af2e40387b9
TinchoCasazza <martin.casazza1@gmail.com>
2018-06-09 19:23:06 -0300

    Tabla

--- grupo02.py
+++ grupo02.py
@@ -1,6 +1,7 @@
 import re
 
 lista_gramatica = []
+tabla = []
 
 class ReglaGramatica():
 
@@ -109,10 +110,31 @@ def setear_gramatica(gramatica_str):
 
     contador = 0
     for gramatica in lista_gramatica:
-        print("R",contador,") " "\x1b[1;33m"+"Gramatica:"+'\033[0;m', mylist[contador], "\x1b[1;32m"+" First: "+'\033[0;m',gramatica.firsts, "\033[1;31m"" Folows:"+'\033[0;m', gramatica.follows, "\x1b[1;36m"" Selects: "+'\033[0;m', gramatica.selects)
+        print("R",contador,") " "\x1b[1;33m"+"Gramatica:"+'\033[0;m', mylist[contador], "\x1b[1;32m"+" First: "+'\033[0;m',gramatica.firsts, "\033[1;31m"" Follows:"+'\033[0;m', gramatica.follows, "\x1b[1;36m"" Selects: "+'\033[0;m', gramatica.selects)
         contador = contador + 1 
 
+    tabla.append(["||"])
+    for x in lista_gramatica:
+        if x.Terminal not in tabla:
+            tabla.append(x.Terminal)
+    
+    contador2 = 0
+    while contador2 < len(lista_gramatica):
+        if lista_gramatica[contador2].reglaGramatica[0] not in tabla[0]:
+            tabla[0].append(lista_gramatica[contador2].reglaGramatica[0])
+        contador2 = 1 + contador2
+    
 
-print(setear_gramatica("A : b A \n A : a \n A : A B c \n A : lambda \n B : b"))
+print(setear_gramatica("A : b A \n A : a \n A : B c \n A : lambda \n B : b"))
+
+print()
+print("\x1b[1;37m"+"Tabla:"+'\033[0;m')
+for x in tabla:
+    print(x)
+
 """print(setear_gramatica("S : X Y Z \n X : a \n X : b \n X : lambda \n Y : a \n Y : d \n Y : lambda \n Z : e \n Z : f \n Z : lambda"))"""
 """print(setear_gramatica("S : a S \n S : b \n S : T J \n S : lambda \n T : c T \n T : d \n J : j J \n J : k"))"""
+
+
+""" ponemos lambda cuando no viene mas nada a la derecha"""
+
