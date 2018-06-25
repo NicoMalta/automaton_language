47f0673a59a862ada8205f5be5a7cee3c48c6f3d
NicoMalta <niicou96@hotmail.com>
2018-06-25 12:15:46 -0300

    Follows 1.0

--- grupo02.py
+++ grupo02.py
@@ -1,7 +1,6 @@
 import re
 
 lista_gramatica = []
-tabla = []
 
 class ReglaGramatica():
 
@@ -66,31 +65,52 @@ def setear_gramatica(gramatica_str):
                         if g < len(gram.reglaGramatica):
                             char_terminal = gram.reglaGramatica[g]
 
-                        
+    lista_gramatica[0].follows.append("$")      
     for gram in lista_gramatica:
         no_terminal = gram.NoTerminal[0]
+        agregarFo = False
+        c = 0
         for gram_aux in lista_gramatica:
             i=2
             while i < len(gram_aux.reglaGramatica):
-                if gram_aux.reglaGramatica[i] == no_terminal:
-                    if i + 1 == len(gram_aux.reglaGramatica) and gram_aux.reglaGramatica[i].isupper() == True:
-                        for follows in gram_aux.follows:
-                            if follows not in gram.follows:
-                                gram.follows.append(follows)               
-                    elif gram_aux.reglaGramatica[i+1].isupper() == True and gram_aux.reglaGramatica[i+1] == gram_aux.reglaGramatica[0]:
+                if gram_aux.reglaGramatica[i] == no_terminal: 
+                    if i+1 < len(gram_aux.reglaGramatica) and gram_aux.reglaGramatica[0] != gram_aux.reglaGramatica[i+1]:
+                        if gram_aux.reglaGramatica[i+1].isupper() == True:
+                            for aux in lista_gramatica:
+                                if aux.reglaGramatica[0] == gram_aux.reglaGramatica[i+1]:
+                                    if "lambda" in aux.firsts:
+                                        agregarFo = True
+                                        i += 1
+                                        c += 1
+                                        if i+1 == len(gram_aux.reglaGramatica):
+                                            break
+                                       
+                    if i+1 == len(gram_aux.reglaGramatica):
+                        for aux in lista_gramatica:
+                            if aux.reglaGramatica[0] == gram_aux.reglaGramatica[0] or agregarFo == True:
+                                for follows in aux.follows:
+                                    if follows not in gram.follows:
+                                        gram.follows.append(follows)
+                                if agregarFo == True:
+                                    while c > 0:
+                                        i -= 1
+                                        for aux in lista_gramatica:
+                                            if aux.reglaGramatica[0] == gram_aux.reglaGramatica[i+1]:
+                                                for firsts in aux.firsts:
+                                                    if firsts not in gram.follows and firsts != "lambda":
+                                                        gram.follows.append(firsts)                                          
+                                        c -= 1
+                                    agregarFo = False
+                    elif gram_aux.reglaGramatica[i+1].isupper() == False:
+                       gram.follows.append(gram_aux.reglaGramatica[i+1])
+                    elif gram_aux.reglaGramatica[i+1].isupper() == True:
                         for aux in lista_gramatica:
                             if aux.reglaGramatica[0] == gram_aux.reglaGramatica[i+1]:
                                 for firsts in aux.firsts:
-                                    if firsts not in gram.follows:
-                                        gram.follows.append(firsts)
-                    elif gram_aux.reglaGramatica[i+1] != gram_aux.reglaGramatica[0]:
-                            for follows in gram_aux.follows:
-                                if follows not in gram.follows:
-                                    gram.follows.append(follows)                    
-                    elif gram_aux.reglaGramatica[i+1].isupper() == False:
-                        if (gram_aux.reglaGramatica[i+1]) not in gram.follows:
-                            gram.follows.append(gram_aux.reglaGramatica[i+1])
+                                    if firsts not in gram.follows and firsts != "lambda":
+                                        gram.follows.append(firsts)                             
                 i += 1
+
         
 
 
@@ -115,39 +135,12 @@ def setear_gramatica(gramatica_str):
 
     contador = 0
     for gramatica in lista_gramatica:
-        print("R",contador,") " "\x1b[1;33m"+"Gramatica:"+'\033[0;m', mylist[contador], "\x1b[1;32m"+" First: "+'\033[0;m',gramatica.firsts, "\033[1;31m"" Follows:"+'\033[0;m', gramatica.follows, "\x1b[1;36m"" Selects: "+'\033[0;m', gramatica.selects)
+        print("R",contador,") " "\x1b[1;33m"+"Gramatica:"+'\033[0;m', mylist[contador], "\x1b[1;32m"+" First: "+'\033[0;m',gramatica.firsts, "\033[1;31m"" Folows:"+'\033[0;m', gramatica.follows, "\x1b[1;36m"" Selects: "+'\033[0;m', gramatica.selects)
         contador = contador + 1 
 
-    tabla.append(["||"])
-    for x in lista_gramatica:
-        if x.Terminal not in tabla:
-            tabla.append(x.Terminal)
-    
-    contador2 = 0
-    while contador2 < len(lista_gramatica):
-        if lista_gramatica[contador2].reglaGramatica[0] not in tabla[0]:
-            tabla[0].append(lista_gramatica[contador2].reglaGramatica[0])
-        contador2 = 1 + contador2
-    
 
-print(setear_gramatica("A : b A \n A : a \n A : B c \n A : lambda \n B : b"))
-
-print()
-print("\x1b[1;37m"+"Tabla:"+'\033[0;m')
-for x in tabla:
-    print(x)
-
-<<<<<<< HEAD
-"""print(setear_gramatica("A : b A \n A : a \n A : A B c \n A : lambda \n B : b"))"""
+print(setear_gramatica("A : b A \n A : a \n A : A B c \n A : lambda \n B : b"))
 """print(setear_gramatica("S : X Y Z \n X : a \n X : b \n X : lambda \n Y : a \n Y : d \n Y : lambda \n Z : e \n Z : f \n Z : lambda"))"""
 """print(setear_gramatica("S : a S \n S : b \n S : T J \n S : lambda \n T : c T \n T : d \n J : j J \n J : k"))"""
 """print(setear_gramatica("E : T R \n R : + T R \n T : F Y \n Y : * F Y  \n F : ( E )"))"""
-print(setear_gramatica("E : T G \n G : + T G \n G : lambda \n T : G W \n W : * G W \n W : lambda \n F : ( E ) \n F : id"))
-=======
-"""print(setear_gramatica("S : X Y Z \n X : a \n X : b \n X : lambda \n Y : a \n Y : d \n Y : lambda \n Z : e \n Z : f \n Z : lambda"))"""
-"""print(setear_gramatica("S : a S \n S : b \n S : T J \n S : lambda \n T : c T \n T : d \n J : j J \n J : k"))"""
-
-
-""" ponemos lambda cuando no viene mas nada a la derecha"""
-
->>>>>>> 9b22cc7a8d15be5ddff7bda4f7190af2e40387b9
+"""print(setear_gramatica("E : T G \n G : + T G \n G : lambda \n T : F W \n W : * F W \n W : lambda \n F : ( E ) \n F : id"))"""
