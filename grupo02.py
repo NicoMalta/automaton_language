62f97fed1d32f91023266ed8d000bc6de6d1386b
NicoMalta <niicou96@hotmail.com>
2018-06-26 10:50:47 -0300

    Selects

--- grupo02.py
+++ grupo02.py
@@ -1,7 +1,8 @@
 import re
+import operator
 
 lista_gramatica = []
-
+lista_auxiliar = []
 class ReglaGramatica():
 
     def __init__(self):
@@ -12,118 +13,15 @@ class ReglaGramatica():
         self.firsts = []
         self.selects = []
 
-
-def setear_gramatica(gramatica_str):
-    Regla = ReglaGramatica()
-    i = 0 
-    char = ""
-    while i < len(gramatica_str):
-        char = gramatica_str[i]
-        f = i + 1
-        if f > len(gramatica_str):
-            pass
-        else:
-            while f < len(gramatica_str) and gramatica_str[f] != " " and char != " ":
-                char += gramatica_str[f]
-                f = f + 1
-            i = f
-            if char == "\n":
-                lista_gramatica.append(Regla)
-                Regla = ReglaGramatica()
-            elif char != " ":
-                Regla.reglaGramatica.append(char)
-                if char != ":":
-                    if char.isupper() == True:
-                        Regla.NoTerminal.append(char)
-                    else:
-                        Regla.Terminal.append(char)
-                if i == len(gramatica_str):
-                    lista_gramatica.append(Regla)
+class Auxiliar():
+    
+    def __init__(self):
+        self.NoTerminal = ''
+        self.selects = []
 
 
-    for gram in lista_gramatica:
-        i=0
-        while gram.reglaGramatica[i] != ":":
-            i += 1
-        if gram.reglaGramatica[i+1].isupper() == False:
-            gram.firsts.append(gram.reglaGramatica[i+1])
-        else:
-            char_terminal = gram.reglaGramatica[i+1]
-            g = i + 1
-            for aux in lista_gramatica:
-                f = 0
-                if aux.reglaGramatica[f] == char_terminal:
-                    while aux.reglaGramatica[f] != ":":
-                        f += 1
-                    if aux.reglaGramatica[f+1].isupper() == False and aux.reglaGramatica[f+1] != "lambda":
-                        if aux.reglaGramatica[f+1] not in gram.firsts:
-                            gram.firsts.append(aux.reglaGramatica[f+1])
-                    elif aux.reglaGramatica[f+1] == "lambda":
-                        g += 1
-                        if "lambda" not in gram.firsts:
-                            gram.firsts.append("lambda")
-                        if g < len(gram.reglaGramatica):
-                            char_terminal = gram.reglaGramatica[g]
-
-    lista_gramatica[0].follows.append("$")      
-    for gram in lista_gramatica:
-        no_terminal = gram.NoTerminal[0]
-        agregarFo = False
-        c = 0
-        for gram_aux in lista_gramatica:
-            i=2
-            while i < len(gram_aux.reglaGramatica):
-                if gram_aux.reglaGramatica[i] == no_terminal: 
-                    if i+1 < len(gram_aux.reglaGramatica) and gram_aux.reglaGramatica[0] != gram_aux.reglaGramatica[i+1]:
-                        if gram_aux.reglaGramatica[i+1].isupper() == True:
-                            for aux in lista_gramatica:
-                                if aux.reglaGramatica[0] == gram_aux.reglaGramatica[i+1]:
-                                    if "lambda" in aux.firsts:
-                                        agregarFo = True
-                                        i += 1
-                                        c += 1
-                                        if i+1 == len(gram_aux.reglaGramatica):
-                                            break
-                                       
-                    if i+1 == len(gram_aux.reglaGramatica):
-                        for aux in lista_gramatica:
-                            if aux.reglaGramatica[0] == gram_aux.reglaGramatica[0] or agregarFo == True:
-                                for follows in aux.follows:
-                                    if follows not in gram.follows:
-                                        gram.follows.append(follows)
-                                if agregarFo == True:
-                                    while c > 0:
-                                        i -= 1
-                                        for aux in lista_gramatica:
-                                            if aux.reglaGramatica[0] == gram_aux.reglaGramatica[i+1]:
-                                                for firsts in aux.firsts:
-                                                    if firsts not in gram.follows and firsts != "lambda":
-                                                        gram.follows.append(firsts)                                          
-                                        c -= 1
-                                    agregarFo = False
-                    elif gram_aux.reglaGramatica[i+1].isupper() == False:
-                       gram.follows.append(gram_aux.reglaGramatica[i+1])
-                    elif gram_aux.reglaGramatica[i+1].isupper() == True:
-                        for aux in lista_gramatica:
-                            if aux.reglaGramatica[0] == gram_aux.reglaGramatica[i+1]:
-                                for firsts in aux.firsts:
-                                    if firsts not in gram.follows and firsts != "lambda":
-                                        gram.follows.append(firsts)                             
-                i += 1
-
-        
-
-
-    """SELECTS"""
-    for gramatica in lista_gramatica:
-        TodosLosFollows = False
-        if "lambda" in gramatica.firsts:
-            gramatica.selects += gramatica.follows
-        else:
-            gramatica.selects += gramatica.firsts
-
-   
-   
+def tabla(gramatica_str):
+          
     x = 0
     for gramatica in lista_gramatica:
         print("R",x,") " "NoTerminal: ",gramatica.NoTerminal, "Terminal: ",gramatica.Terminal)
@@ -196,6 +94,167 @@ def setear_gramatica(gramatica_str):
     print()
     for x in tabla:
         print(x)
+
+def crearLista(gramatica_str):
+    Regla = ReglaGramatica()
+    i = 0 
+    char = ""
+    while i < len(gramatica_str):
+        char = gramatica_str[i]
+        f = i + 1
+        if f > len(gramatica_str):
+            pass
+        else:
+            while f < len(gramatica_str) and gramatica_str[f] != " " and char != " ":
+                char += gramatica_str[f]
+                f = f + 1
+            i = f
+            if char == "\n":
+                lista_gramatica.append(Regla)
+                Regla = ReglaGramatica()
+            elif char != " ":
+                Regla.reglaGramatica.append(char)
+                if char != ":":
+                    if char.isupper() == True:
+                        Regla.NoTerminal.append(char)
+                    else:
+                        Regla.Terminal.append(char)
+                if i == len(gramatica_str):
+                    lista_gramatica.append(Regla)
+
+def calculoFirsts():
+    for gram in lista_gramatica:
+        i=0
+        while gram.reglaGramatica[i] != ":":
+            i += 1
+        if gram.reglaGramatica[i+1].isupper() == False:
+            gram.firsts.append(gram.reglaGramatica[i+1])
+        else:
+            char_terminal = gram.reglaGramatica[i+1]
+            g = i + 1
+            for aux in lista_gramatica:
+                f = 0
+                if aux.reglaGramatica[f] == char_terminal:
+                    while aux.reglaGramatica[f] != ":":
+                        f += 1
+                    if aux.reglaGramatica[f+1].isupper() == False and aux.reglaGramatica[f+1] != "lambda":
+                        if aux.reglaGramatica[f+1] not in gram.firsts:
+                            gram.firsts.append(aux.reglaGramatica[f+1])
+                    elif aux.reglaGramatica[f+1] == "lambda":
+                        g += 1
+                        if "lambda" not in gram.firsts and g == len(gram.reglaGramatica):
+                            gram.firsts.append("lambda")
+                        if g < len(gram.reglaGramatica):
+                            char_terminal = gram.reglaGramatica[g]
+                    else:
+                        char_terminal = aux.reglaGramatica[f+1]                     
+
+def calculoFollows():
+    lista_gramatica[0].follows.append("$")      
+    for gram in lista_gramatica:
+        no_terminal = gram.NoTerminal[0]
+        agregarFo = False
+        c = 0
+        for gram_aux in lista_gramatica:
+            i=2
+            while i < len(gram_aux.reglaGramatica):
+                if gram_aux.reglaGramatica[i] == no_terminal: 
+                    if i+1 < len(gram_aux.reglaGramatica) and gram_aux.reglaGramatica[0] != gram_aux.reglaGramatica[i+1]:
+                        if gram_aux.reglaGramatica[i+1].isupper() == True:
+                            for aux in lista_gramatica:
+                                if aux.reglaGramatica[0] == gram_aux.reglaGramatica[i+1]:
+                                    if "lambda" in aux.firsts:
+                                        agregarFo = True
+                                        i += 1
+                                        c += 1
+                                        if i+1 == len(gram_aux.reglaGramatica):
+                                            break
+                                       
+                    if i+1 == len(gram_aux.reglaGramatica):
+                        for aux in lista_gramatica:
+                            if aux.reglaGramatica[0] == gram_aux.reglaGramatica[0] or agregarFo == True:
+                                for follows in aux.follows:
+                                    if follows not in gram.follows:
+                                        gram.follows.append(follows)
+                                if agregarFo == True:
+                                    while c > 0:
+                                        i -= 1
+                                        for aux in lista_gramatica:
+                                            if aux.reglaGramatica[0] == gram_aux.reglaGramatica[i+1]:
+                                                for firsts in aux.firsts:
+                                                    if firsts not in gram.follows and firsts != "lambda":
+                                                        gram.follows.append(firsts)                                          
+                                        c -= 1
+                                    agregarFo = False
+                    elif gram_aux.reglaGramatica[i+1].isupper() == False:
+                       gram.follows.append(gram_aux.reglaGramatica[i+1])
+                    elif gram_aux.reglaGramatica[i+1].isupper() == True:
+                        for aux in lista_gramatica:
+                            if aux.reglaGramatica[0] == gram_aux.reglaGramatica[i+1]:
+                                for firsts in aux.firsts:
+                                    if firsts not in gram.follows and firsts != "lambda":
+                                        gram.follows.append(firsts)                             
+                i += 1
+    for gram in lista_gramatica:
+        if len(gram.follows) == 0:
+            gram.follows.append("$")
+
+def calculoSelects():
+    for gramatica in lista_gramatica:
+        if "lambda" in gramatica.firsts:
+            for auxfirsts in gramatica.firsts:
+                if auxfirsts != "lambda":
+                    gramatica.selects.append(auxfirsts)
+            gramatica.selects += gramatica.follows           
+        else:
+            gramatica.selects += gramatica.firsts
+
+def determinarLL1():
+    EsLL1 = True
+    auxiliar = Auxiliar()
+    auxiliar.NoTerminal = lista_gramatica[0].reglaGramatica[0] 
+    lista_auxiliar.append(auxiliar)
+    for gram in lista_gramatica:
+        if auxiliar.NoTerminal == gram.reglaGramatica[0]:
+            auxiliar.selects += gram.selects
+        else:
+            auxiliar = Auxiliar()
+            auxiliar.NoTerminal = gram.reglaGramatica[0]
+            auxiliar.selects += gram.selects
+            lista_auxiliar.append(auxiliar)
+    for aux in lista_auxiliar:
+        aux_dict = {i:aux.selects.count(i) for i in aux.selects}
+        if max(aux_dict.items(), key=operator.itemgetter(1))[1] > 1:
+            EsLL1 = False
+            break
+    return EsLL1
+
+def setear_gramatica(gramatica_str):
+    """Agrega gramaticas a la lista y divide por terminales y no terminales"""
+    crearLista(gramatica_str)
+
+    """FIRSTS"""
+    calculoFirsts()
+                        
+    """FOLLOWS"""
+    calculoFollows()
+
+    """SELECTS"""
+    calculoSelects()
+
+    """Determino si es LL1"""
+    EsLL1 = determinarLL1()
+
+    
+
+    """IMPRIMIR TABLA"""
+    tabla(gramatica_str)
+
+    if EsLL1 == True:
+        print("Es LL1")
+    else:
+        print("No es LL1")
+
     
 
 
